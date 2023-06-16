9676ed3f4d4c30fd33ac5ec1dfe3d07328a5d838
Chris Candelora <chris@inigus.com>
2023-06-16 13:39:18 -0400

    adding heroku profile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
